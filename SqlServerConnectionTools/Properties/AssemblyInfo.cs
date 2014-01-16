129beb87de01ecb43630aa7d51917366ef96cd7c
salar2k <salar2k@gmail.com>
2014-01-16 09:38:03 +0330

    Version update

--- SqlServerConnectionTools/Properties/AssemblyInfo.cs
+++ SqlServerConnectionTools/Properties/AssemblyInfo.cs
@@ -1,36 +1,19 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SqlServerConnectionGenerator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SqlServerConnectionTools")]
+[assembly: AssemblyDescription("SqlServerConnectionTools by Salar Khalilzadeh")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SalarSoft")]
-[assembly: AssemblyProduct("SqlServerConnectionGenerator")]
-[assembly: AssemblyCopyright("Copyright © SalarSoft 2012")]
+[assembly: AssemblyCompany("Salar Khalilzadeh <salar2k@gmail.com>")]
+[assembly: AssemblyProduct("SqlServerConnectionTools")]
+[assembly: AssemblyCopyright("Copyright © Salar Khalilzadeh 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4e41b7b9-a8be-46a0-ad44-26f77b8a943b")]
 
 // Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
